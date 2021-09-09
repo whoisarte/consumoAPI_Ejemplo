91f7c2b41c5ecc077eddd78a09411e66b730a6da
Artemio Pánuco <arte.solo.arte@hotmail.com>
2021-09-09 10:07:33 -0500

    Initial Commit

---
+++ consumoAPI_Ejemplo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  consumoAPI_Ejemplo
+//
+//  Created by Artemio on 09/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
